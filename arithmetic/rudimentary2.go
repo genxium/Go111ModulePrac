ce3036e057c8bdd2cf279f5b6970dd8353e5a9f0
genxium <genxium@hotmail.com>
2019-03-22 18:58:55 +0800

    Formatted.

--- arithmetic/rudimentary2.go
+++ arithmetic/rudimentary2.go
@@ -1,9 +1,9 @@
 package arithmetic
 
 func Multiply(x int, y int) int {
-  return x * y
+	return x * y
 }
 
 func Divide(y int, byX int) int {
-  return y / byX
+	return y / byX
 }
--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "mycompany.org/thisproj/arithmetic"
-  "strconv"
-  "fmt"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"mycompany.org/thisproj/arithmetic"
+	"strconv"
 )
 
 type Res struct {
-  Ret int `json:"ret"`
-  Sum int `json:"sum"`
+	Ret int `json:"ret"`
+	Sum int `json:"sum"`
 }
 
 const (
-  OK = 1000
+	OK = 1000
 )
 
 func main() {
-  router := gin.Default()
+	router := gin.Default()
 	router.GET("v:version/test-api", func(c *gin.Context) {
 		defer func() {
 			if r := recover(); r != nil {
@@ -25,20 +25,20 @@ func main() {
 			}
 		}()
 		c.Header("Content-Type", "application/json")
-    a, aerr := strconv.Atoi(c.Query("a"))
-    if aerr != nil {
-      panic(aerr)
-    }
+		a, aerr := strconv.Atoi(c.Query("a"))
+		if aerr != nil {
+			panic(aerr)
+		}
 		b, berr := strconv.Atoi(c.Query("b"))
-    if berr != nil {
-      panic(berr)
-    }
-    sum := arithmetic.Add(a, b)
+		if berr != nil {
+			panic(berr)
+		}
+		sum := arithmetic.Add(a, b)
 
 		r := Res{
-      Ret: OK,
-      Sum: sum,
-    }
+			Ret: OK,
+			Sum: sum,
+		}
 
 		c.JSON(200, r)
 	})
